d6c87a77a036e06aa886ba3411436ca7f58452b2
Naveen <naveen.chaurasia861@gmail.com>
2020-12-25 20:32:47 +0530

    initial commit

---
+++ spring-hoverfly/src/main/java/com/hoverfly/springhoverfly/SpringHoverflyApplication.java
@@ -0,0 +1,63 @@
+package com.hoverfly.springhoverfly;
+ 
+import java.util.Date;
+import java.util.UUID;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@SpringBootApplication
+public class SpringHoverflyApplication {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHoverflyApplication.class, args);
+    }
+}
+ 
+@RestController
+class MyRestController {
+ 
+    @RequestMapping(value = "/service/hoverfly")
+    public HoverflyServiceResponse getSampleResponse() {
+        System.out.println("Inside HoverflyActualServiceApplication::getSampleResponse()");
+        return new HoverflyServiceResponse("returned value from HoverflyActualServiceApplication", new Date().toString(), UUID.randomUUID().toString());
+    }
+}
+ 
+class HoverflyServiceResponse {
+    private String message;
+    private String responseTime;
+    private String transactionid;
+ 
+    public HoverflyServiceResponse(String message, String responseTime, String transactionid) {
+        super();
+        this.message = message;
+        this.responseTime = responseTime;
+        this.transactionid = transactionid;
+    }
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+ 
+    public String getResponseTime() {
+        return responseTime;
+    }
+ 
+    public void setResponseTime(String responseTime) {
+        this.responseTime = responseTime;
+    }
+ 
+    public String getTransactionid() {
+        return transactionid;
+    }
+ 
+    public void setTransactionid(String transactionid) {
+        this.transactionid = transactionid;
+    }
+}
---
+++ spring-hoverfly/src/test/java/com/hoverfly/springhoverfly/SpringHoverflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoverfly.springhoverfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHoverflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
