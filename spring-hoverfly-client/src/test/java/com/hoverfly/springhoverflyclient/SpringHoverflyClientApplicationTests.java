15c065def1ff72823a04e1cfe3c99fe99a106d3e
Naveen <naveen.chaurasia861@gmail.com>
2020-12-25 22:20:55 +0530

    added client

---
+++ spring-hoverfly-client/src/test/java/com/hoverfly/springhoverflyclient/SpringHoverflyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoverfly.springhoverflyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHoverflyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
